63b3d6ecc8fae9b0a5468a155c3c6a792c1de7d1
Charlie Aleck <aleckcharlie@gmail.com>
2021-08-27 14:46:30 -0500

    book and user typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,25 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    title: String
+    link: String
+  }
+  type Auth {
+    token: ID! 
+    user: User
+  }
   type Query {
     me: User
   }
